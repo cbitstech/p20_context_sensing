690e76c90f478af9f7b8850fd96c837b94192b34
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2015-01-05 13:03:29 -0600

    changes by Mark on Scott's computer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,13 +13,13 @@
   //  var launchRoute = function(){(new PurpleRobot()).fetchEncryptedString('gotoOnAppStart').execute({
   //    done: function(payload){
   //     $scope.gotoOnAppStart = payload;}
-  //     , 
-  //     fail: function(error){ 
+  //     ,
+  //     fail: function(error){
     //    console.log(error);
     //    $scope.gotoOnAppStart = '/';
     //  }
     // });}
-  
+
   // PurpleRobot = {};
 
   $scope.showHome = true;
@@ -50,21 +50,26 @@
 
     var amTriggerDateTime = new Date();
     var pmTriggerDateTime = new Date();
-    
+    var amTriggerDateTimeEnd = new Date();
+    var pmTriggerDateTimeEnd = new Date();
+
     amTriggerDateTime.setHours($("input[name=amprompttime").val().split(":")[0]);
     amTriggerDateTime.setMinutes($("input[name=amprompttime").val().split(":")[1]);
     pmTriggerDateTime.setHours($("input[name=pmprompttime").val().split(":")[0]);
     pmTriggerDateTime.setMinutes($("input[name=pmprompttime").val().split(":")[1]);
 
+    amTriggerDateTimeEnd.setHours($("input[name=amprompttime").val().split(":")[0]);
+    amTriggerDateTimeEnd.setMinutes(parseInt($("input[name=amprompttime").val().split(":")[1])+1);
+    pmTriggerDateTimeEnd.setHours($("input[name=pmprompttime").val().split(":")[0]);
+    pmTriggerDateTimeEnd.setMinutes(parseInt($("input[name=pmprompttime").val().split(":")[1])+1);
+
+
+
     console.log(amTriggerDateTime,pmTriggerDateTime);
-
-
-    var amTriggerTime = (new PurpleRobot()).formatDate(amTriggerDateTime).toString();
-    var pmTriggerTime = (new PurpleRobot()).formatDate(pmTriggerDateTime).toString();
 
     var pr = new PurpleRobot();
 
-    var dialog = 
+    var dialog =
     pr.showNativeDialog({
       title: "P20",
       message: "Can you answer a few questions?",
@@ -78,8 +83,8 @@
 
 
 
-    (new PurpleRobot()).updateTrigger({ script: dialog, startAt: amTriggerTime, endAt: amTriggerTime}).execute();
-    (new PurpleRobot()).updateTrigger({ script: dialog, startAt: pmTriggerTime, endAt: pmTriggerTime}).execute();
+    (new PurpleRobot()).updateTrigger({ script: dialog, startAt: amTriggerDateTime, endAt: amTriggerDateTimeEnd}).execute();
+    (new PurpleRobot()).updateTrigger({ script: dialog, startAt: pmTriggerDateTime, endAt: pmTriggerDateTimeEnd}).execute();
 
 
     $location.path('#/');
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/markbegale/projects/p20_context_sensing/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/markbegale/projects/p20_context_sensing/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/markbegale/projects/p20_context_sensing/platforms/android/ant-build/classes.dex \
-/Users/markbegale/projects/p20_context_sensing/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/cambo/Documents/p20_context_sensing/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/cambo/Documents/p20_context_sensing/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/cambo/Documents/p20_context_sensing/platforms/android/ant-build/classes.dex \
+/Users/cambo/Documents/p20_context_sensing/platforms/android/CordovaLib/ant-build/classes.jar \
